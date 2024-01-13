72a83a7cfe86a328e7e05ec26b6f8624b79ece43
Francisco Hernandez <francj.hdez@gmail.com>
2024-01-13 14:53:22 -0600

    In the file 0-console.js, create a function named displayMessage that prints in STDOUT the string argument.

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,3 +1,10 @@
-module.exports = function displayMessage(text) {
-  console.log(text);
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Sunday Jimoh <https://github.com/sunday4me>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
 };
+
+module.exports = displayMessage;
